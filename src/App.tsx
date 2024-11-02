a77e748d27faf217edb45e485c730110eca6efb0
Ketan Jain <ketanjainn05@gmail.com>
2024-11-03 00:11:42 +0530

    'card-initial-design'

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import Card from './components/Card'
 function App() {
   return (
     <>
-      <Card
-        authorName={`Ketan Jain`}
-        feedback={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus officia ea sed laborum pariatur deleniti ullam nam placeat, quisquam modi alias sequi odio architecto aspernatur libero, at quidem rem adipisci!`}
+      <div className='p-20'>
+        <Card
+          authorName={`Ketan Jain`}
+          feedback={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus officia ea sed laborum pariatur deleniti ullam nam placeat, quisquam modi alias sequi odio architecto aspernatur libero, at quidem rem adipisci!`}
+          designation='Head of Customer Marketing '
+          profilePicture='src/assets/pfp.jpeg'
+          companyLogo='https://www.luckymedia.dev/_next/image?url=https%3A%2F%2Fcms.luckymedia.dev%2Fassets%2Fposts%2Ftailwind-css-logo-post.png&w=3840&q=75'
+        />
+      </div>
 
-      />
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,20 +3,27 @@ interface CardProps {
     designation?: string;
     feedback: string;
     companyLogo?: string;
+    profilePicture?: string
 }
 
-const Card: React.FC<CardProps> = ({ 
-    authorName, 
-    designation, 
-    feedback, 
-    companyLogo 
+const Card: React.FC<CardProps> = ({
+    authorName,
+    designation,
+    feedback,
+    companyLogo,
+    profilePicture
 }) => {
     return (
-        <div className="bg-black text-white w-24 h-24 rounded-lg">
-            <h3>{authorName}</h3>
-            {designation && <p>{designation}</p>}
-            <p>{feedback}</p>
-            {companyLogo && <img src={companyLogo} alt="Company logo" />}
+        <div className=" bg-[#25282c] hover:bg-[#33363a] border-[#282955] border-[1px] rounded-lg p-6 max-h-[350px] max-w-[372px] w-max shadow-[#282955] shadow-md">
+            <div className="flex flex-row mb-4">
+                <img src={profilePicture} alt="pfp_here" className="rounded-full size-12" />
+                <div className="flex flex-col pl-2">
+                    <p className="text-bold text-base font-bold text-gray-200">{authorName}</p>
+                    {designation && <p className="text-gray-300 text-sm">{designation}</p>}
+                </div>
+            </div>
+            <p className="text-gray-200">{feedback}</p>
+            {companyLogo && <img className="h-20" src={companyLogo} alt="Company logo" />}
         </div>
     );
 };
